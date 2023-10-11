936675a05dd0bee248787b11778785bfa37c2b2f
Ben Johnson <monk.sound.design@gmail.com>
2023-10-11 12:09:34 -0400

    welcome imports

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from "react";
+import {
+  View, //div
+  Text, // p
+  TextInput, // input
+  TouchableOpacity, // button
+  Image, // image
+  FlatList, // map for large, easily scrollable lists
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './welcome.style'
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
       <Text>Welcome</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
