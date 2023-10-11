2875c7a5c8c756fb61693f5be246e9893080c9e5
Ben Johnson <monk.sound.design@gmail.com>
2023-10-11 12:29:20 -0400

    custom font support

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -17,7 +17,16 @@ const Welcome = () => {
 
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello, Ben</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job.</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} />
+        </View>
+      </View>
     </View>
   );
 };
