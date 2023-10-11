d99a772c381e83c280b8b7d9078cc574ccaac9c1
Ben Johnson <monk.sound.design@gmail.com>
2023-10-11 14:30:44 -0400

    render job data on screen in job cards

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { RAPID_API_KEY } from "@env";
-
-const rapidApiKey = RAPID_API_KEY;
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -12,7 +9,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": rapidApiKey,
+      "X-RapidAPI-Key": "250a262311msh6237b0b51f0903bp1c6d6cjsn9789d3d17a01",
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
@@ -32,6 +29,17 @@ const useFetch = (endpoint, query) => {
       setIsLoading(false);
     }
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
 };
 
 export default useFetch;
