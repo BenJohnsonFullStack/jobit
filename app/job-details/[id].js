f03733e4216e2b88e71232b4d05a62f8cd831c28
Ben Johnson <monk.sound.design@gmail.com>
2023-10-11 19:54:37 -0400

    job details imports and initial variables

---
+++ app/job-details/[id].js
@@ -0,0 +1,35 @@
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+import { Stack, useRouter, useGlobalSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
+
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components";
+
+import { COLORS, SIZES, icons } from "../../constants";
+import useFetch from "../../hooks/useFetch";
+
+const JobDetails = () => {
+  const params = useGlobalSearchParams();
+  const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch("job-details", {
+    job_id: params.id,
+  });
+
+  return <Text>JobDetails</Text>;
+};
+
+export default JobDetails;
