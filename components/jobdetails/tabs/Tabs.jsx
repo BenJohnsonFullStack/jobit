61287aa6cb0feb47410611f0d87309177ce5f933
Ben Johnson <monk.sound.design@gmail.com>
2023-10-12 00:32:39 -0400

    job detail buttons

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -4,8 +4,11 @@ import styles from "./tabs.style";
 import { SIZES } from "../../../constants";
 
 const TabButton = ({ name, activeTab, onHandleSearchType }) => (
-  <TouchableOpacity>
-    <Text>{name}</Text>
+  <TouchableOpacity
+    style={styles.btn(name, activeTab)}
+    onPress={onHandleSearchType}
+  >
+    <Text style={styles.btnText(name, activeTab)}>{name}</Text>
   </TouchableOpacity>
 );
 
@@ -14,6 +17,10 @@ const Tabs = ({ tabs, activeTab, setActiveTab }) => {
     <View style={styles.container}>
       <FlatList
         data={tabs}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item}
+        contentContainerStyle={{ columnGap: SIZES.small / 2 }}
         renderItem={({ item }) => (
           <TabButton
             name={item}
